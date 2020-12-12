50d95d0e1b0954bf342e10cd5dbd7830996425af
Moritz Kneilmann <moritz.kneilmann@gmx.de>
2020-12-12 11:25:18 +0100

    Orchestator: Add new ssh key
    [skip-ci]

--- orchestrator/src/config.js
+++ orchestrator/src/config.js
@@ -14,6 +14,7 @@ module.exports = {
   vmSshKeys: [
     "a3:68:a6:33:c8:d9:4b:b4:e1:cd:91:d7:72:01:69:01",
     "3e:b7:e7:14:f0:c7:0d:49:7a:f3:5b:7b:65:b4:d3:0f",
+    "46:17:b4:bd:7d:bf:f1:4e:40:de:e3:d8:77:a3:bf:41",
   ],
   vmTags: ["7dfps-game-vm"],
 };
